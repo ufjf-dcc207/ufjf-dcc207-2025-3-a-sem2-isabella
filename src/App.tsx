ddc4c5eb70f5a724d4f8bbf22ebaba563b73fc2a
Isabella Gonçalves <goncalves.isabella@estudante.ufjf.br>
2025-11-04 16:49:15 -0300

    :art: esboço do sistema

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,17 @@ import './App.css'
 
 function App() {
   return <div className="app">
-    <Disciplina />
+    <Disciplina nome="Algoritmos I - teórica" carga={4} aval="⭐⭐⭐⭐" lab={false}/>
+    <Disciplina nome="Algoritmos I - prática" carga={2} aval="⭐⭐⭐⭐⭐" lab={true}/>
+    <Disciplina nome="Algoritmos II - teórica" carga={4} aval="⭐⭐⭐⭐" lab={false}/>
+    <Disciplina nome="Algoritmos II - prática" carga={2} aval="⭐⭐⭐⭐⭐" lab={true}/>
+    <Disciplina nome="Desenvolvimento Web I" carga={2} aval="⭐⭐⭐⭐" lab={true}/>
+    <Disciplina nome="Desenvolvimento Web II" carga={2} aval="⭐⭐⭐⭐" lab={true}/>
+    <Disciplina nome="Organização de Computadores" carga={4} aval="⭐⭐" lab={false}/>
+    <Disciplina nome="Circuitos Digitais" carga={4} aval="⭐⭐⭐⭐" lab={false}/>
+    <Disciplina nome="Calculo 1" carga={4} aval="⭐⭐⭐" lab={false}/>
+    <Disciplina nome="Calculo 2" carga={4} aval="⭐⭐⭐" lab={false}/>
+    <Disciplina nome="Aspectos Legais da Informática" carga={2} aval="⭐⭐⭐⭐" lab={false}/>
   </div>;
 }
 

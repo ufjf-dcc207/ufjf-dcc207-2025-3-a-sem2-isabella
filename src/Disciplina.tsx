0159b0782783911620e06ea93e124cc07a5d876d
Isabella Gonçalves <goncalves.isabella@estudante.ufjf.br>
2025-11-06 09:51:41 -0300

    :art: melhoras no visual

--- src/Disciplina.tsx
+++ src/Disciplina.tsx
@@ -17,8 +17,14 @@ export default function Disciplina({
         <div className="disciplina">
             <div className="nome">{nome}</div>
             <div className="carga">{carga.toLocaleString()}hr</div>
+            {aval > 4.2 ? (
+                <div className="aval">Avaliação:{aval.toLocaleString()}</div>
+            ) : (
+                <div className="avalBaixa">Avaliação:{aval.toLocaleString()}</div>
+            )}
             <div className="avaliacao">{aval}</div>
-            <div className="lab">{lab}</div>
+            {lab && <div className="lab">LABORATÓRIO</div>}
+            
         </div>
     )
 }
